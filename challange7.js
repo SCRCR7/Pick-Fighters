68007967a50c59b7681dfe191aa873c550d75e09
Sohaib Hassan <93832203+SCRCR7@users.noreply.github.com>
2023-08-12 01:07:39 +0500

    Pick Fighter Game

---
+++ challange7.js
@@ -0,0 +1,14 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function random() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    frandom=Math.floor(Math.random()*fighters.length)
+    srandom=Math.floor(Math.random()*fighters.length)
+    stageEl.textContent=fighters[frandom]+"VS"+fighters[srandom]
+})
+
